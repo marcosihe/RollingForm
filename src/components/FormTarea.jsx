5bc6f7eb4578b64ea73bf9c834491daec0604681
marcosihe <marcosherrera.ib@gmail.com>
2021-06-18 12:42:06 -0300

    Se agrego la funcionalidad de capturar y agregar las tareas en un arreglo, ademas de limpiar el campo del input haciendo uso del evento onSubmit.

--- src/components/FormTarea.jsx
+++ src/components/FormTarea.jsx
@@ -4,22 +4,30 @@ const FormTarea = () => {
 
 const [tarea, setTarea] = useState('');
 const [listaTareas, setListaTareas] = useState([]);
+
 //const guardarTarea = (e) => {
 //    setTarea(e.target.value);
 //    console.log(tarea);
 //}
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setListaTareas([...listaTareas,tarea]);
+  setTarea(''); // Para borrar el valor del state y por ende limpiar el input
+}
+
   return (
     <div className='container my-5 d-flex justify-content-center'>
-      <form className='w-75'>
+      <form className='w-75' onSubmit={handleSubmit}>
         <div className='d-flex mt-2 mb-2'>
           <input
             type='text'
             placeholder='Ingrese una tarea'
             className='form-control'
             onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
           />
-          <button className='btn btn-outline-light'>Agregar</button>
+          <button className='btn btn-outline-light' type='submit'>Agregar</button>
         </div>
       </form>
     </div>
